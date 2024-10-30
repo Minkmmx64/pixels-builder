7b623d9c18fba8c31358233e739617e2e4962269
2268731813@qq.com <2268731813@qq.com>
2024-10-30 16:27:30 +0800

    打包&electron使用

--- src/App.vue
+++ src/App.vue
@@ -2,6 +2,18 @@
   <router-view />
 </template>
 
+<script lang="ts" setup>
+import { onMounted } from 'vue';
+
+
+onMounted(() => {
+
+})
+</script>
+
 <style lang="less">
-* { padding: 0px; margin: 0px; }
+* {
+  padding: 0px;
+  margin: 0px;
+}
 </style>
